4f188d6459ed1e521bca74202fc4efefc11a1fc2
pavel.tokarev <pavel.tokarev@mail.ntc51>
2023-04-06 11:17:28 +0300

    Добавил Capacity/calculateCapacity.m и Capacity/createQuaDRiGa.m

---
+++ Capacity/privateFunc/createKroneckerChannels.m
@@ -0,0 +1,8 @@
+function Hk = createKroneckerChannels(numTx,numRx,numChan,R,Z)
+    Hk = zeros(numRx,numTx,numChan);
+    for k = 1:numChan
+        H = createStaticChannel(numTx,numRx);
+        H = H.';
+        Hk(:,:,k) = H*sqrt(R)*Z;
+    end
+end
