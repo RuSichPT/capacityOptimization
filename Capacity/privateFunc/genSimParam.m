86eb781f97270e2317a63f1018aebf53482971dd
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-04-25 16:07:18 +0300

    Удаляем лишнее

--- Capacity/privateFunc/genSimParam.m
+++
@@ -1,7 +0,0 @@
-function s = genSimParam(c_freq)
-    s = qd_simulation_parameters;               % Set general simulation parameters
-    s.center_frequency = c_freq;                   % Set center frequency for the simulation
-    s.use_3GPP_baseline = 1;                    % Disable spherical waves
-    s.show_progress_bars = 0;                   % Disable progress bar
-end
-
